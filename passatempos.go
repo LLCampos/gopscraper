84214731794bf34642a740154006b1982b89851e
LLCampos <luis1212@sapo.pt>
2017-07-30 14:03:35 +0100

    Merge contests data

--- passatempos.go
+++ passatempos.go
@@ -7,26 +7,29 @@ import (
 )
 
 func main() {
-	println(getContestsFromPage(
+	artefactos_contests := getContestsFromPage(
 		"artefactos",
 		"http://www.arte-factos.net/passatempos-af/",
 		"div.titulo-pagina > a",
 		getContestsArtefactos,
-	))
+	)
 
-	println(getContestsFromPage(
+	transporteslisboa_contests := getContestsFromPage(
 		"transporteslisboa",
 		"http://passatempos.transporteslisboa.pt/",
 		"h1.entry-title > a",
 		getContestsTransporteslisboa,
-	))
+	)
+
+	all_contests := mergeMaps(artefactos_contests, transporteslisboa_contests)
+	println(convertToJson(all_contests))
 }
 
+type pageContestsData []map[string]string
+type contestsData map[string]pageContestsData
+type getDataFromContestsElemFunc func(*goquery.Selection, pageContestsData) pageContestsData
 
-type arrayContestsData []map[string]string
-type getDataFromContestsElemFunc func(*goquery.Selection, arrayContestsData) arrayContestsData
-
-func getContestsFromPage(page_name string, page_url string, contests_element_path string, getDataFromContestsElem getDataFromContestsElemFunc) string {
+func getContestsFromPage(page_name string, page_url string, contests_element_path string, getDataFromContestsElem getDataFromContestsElemFunc) contestsData {
 	log.Println("Scraping", page_name)
 
 	doc, err := goquery.NewDocument(page_url)
@@ -39,17 +42,17 @@ func getContestsFromPage(page_name string, page_url string, contests_element_pat
 	contests_elem := doc.Find(contests_element_path)
 	number_contests := contests_elem.Length()
 
-	contests := make(arrayContestsData, number_contests)
+	contests := make(pageContestsData, number_contests)
 
 	contests = getDataFromContestsElem(contests_elem, contests)
 
-	contests_map := make(map[string]arrayContestsData)
-	contests_map[page_name] = contests
+	page_contests := make(contestsData)
+	page_contests[page_name] = contests
 
-	return convertToJson(contests_map)
+	return page_contests
 }
 
-func getContestsArtefactos(contests_elem *goquery.Selection, contests arrayContestsData) arrayContestsData {
+func getContestsArtefactos(contests_elem *goquery.Selection, contests pageContestsData) pageContestsData {
 	contests_elem.Each(func(i int, s *goquery.Selection) {
 		contest_map := make(map[string]string)
 		contest_map["name"] = s.Find("h4").Text()
@@ -59,7 +62,7 @@ func getContestsArtefactos(contests_elem *goquery.Selection, contests arrayConte
 	return contests
 }
 
-func getContestsTransporteslisboa(contests_elem *goquery.Selection, contests arrayContestsData) arrayContestsData {
+func getContestsTransporteslisboa(contests_elem *goquery.Selection, contests pageContestsData) pageContestsData {
 	contests_elem.Each(func(i int, s *goquery.Selection) {
 		contest_map := make(map[string]string)
 		contest_map["name"] = s.Text()
@@ -70,10 +73,17 @@ func getContestsTransporteslisboa(contests_elem *goquery.Selection, contests arr
 }
 
 
-func convertToJson(to_convert map[string]arrayContestsData) string {
+func convertToJson(to_convert contestsData) string {
 	bytes, err := json.Marshal(to_convert)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return string(bytes)
+}
+
+func mergeMaps(map1 contestsData, map2 contestsData) contestsData {
+	for k, v := range map1 {
+		map2[k] = v
+	}
+	return map2
 }
