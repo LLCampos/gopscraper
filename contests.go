a21c548887f3a630e3abab3c42489f4a8398a2d0
LLCampos <luis1212@sapo.pt>
2017-08-05 18:57:35 +0100

    Fix "activa sapo" bot and refactor

--- contests.go
+++ contests.go
@@ -4,6 +4,7 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"encoding/json"
+	"strings"
 )
 
 
@@ -17,7 +18,7 @@ func GetContests() string{
 		"activa sapo",
 		"http://activa.sapo.pt/passatempos/",
 		"div.textDetails > h1.title > a",
-		getContestsActivaSapo,
+		getDataFromContestsElemRelativeURL,
 		ch,
 	)
 
@@ -33,15 +34,25 @@ func GetContests() string{
 		"antena3",
 		"http://media.rtp.pt/antena3/passatempos/",
 		"h3.entry-title > a",
-		getContestsAntena3,
+		getDataFromContestsElemAbsoluteURL,
 		ch,
 	)
+
+	/*
+	go getContestsFromPage(
+		"axn",
+		"http://www.axn.pt/contests",
+		"div.promo-teaser.snippet.contest > h2 > a",
+		getContestsAXN,
+		ch,
+	)
+	*/
 
 	go getContestsFromPage(
 		"transporteslisboa",
 		"http://passatempos.transporteslisboa.pt/",
 		"h1.entry-title > a",
-		getContestsTransporteslisboa,
+		getDataFromContestsElemAbsoluteURL,
 		ch,
 	)
 
@@ -57,7 +68,7 @@ func GetContests() string{
 
 type pageContestsData []map[string]string
 type contestsData map[string]pageContestsData
-type getDataFromContestsElemFunc func(*goquery.Selection, pageContestsData) pageContestsData
+type getDataFromContestsElemFunc func(*goquery.Selection, pageContestsData, string) pageContestsData
 
 func getContestsFromPage(page_name string, page_url string, contests_element_path string, getDataFromContestsElem getDataFromContestsElemFunc, ch chan contestsData) {
 	log.Println("Downloading", page_url)
@@ -74,7 +85,7 @@ func getContestsFromPage(page_name string, page_url string, contests_element_pat
 
 	contests := make(pageContestsData, number_contests)
 
-	contests = getDataFromContestsElem(contests_elem, contests)
+	contests = getDataFromContestsElem(contests_elem, contests, page_url)
 	log.Println("Finished Scraping ", page_url)
 
 	page_contests := make(contestsData)
@@ -83,17 +94,31 @@ func getContestsFromPage(page_name string, page_url string, contests_element_pat
 	ch <- page_contests
 }
 
-func getContestsActivaSapo(contests_elem *goquery.Selection, contests pageContestsData) pageContestsData {
+func getDataFromContestsElemAbsoluteURL(contests_elem *goquery.Selection, contests pageContestsData, page_url string) pageContestsData {
+	return getDataFromContestsElemBase(contests_elem , contests, page_url, false)
+}
+
+func getDataFromContestsElemRelativeURL(contests_elem *goquery.Selection, contests pageContestsData, page_url string) pageContestsData {
+	return getDataFromContestsElemBase(contests_elem , contests, page_url, true)
+}
+
+func getDataFromContestsElemBase(contests_elem *goquery.Selection, contests pageContestsData, page_url string, relative_url bool) pageContestsData {
 	contests_elem.Each(func(i int, s *goquery.Selection) {
 		contest_map := make(map[string]string)
 		contest_map["name"] = s.Text()
-		contest_map["url"], _ = s.Attr("href")
+		contest_url, _ := s.Attr("href")
+		if relative_url {
+			contest_map["url"] = getURLroot(page_url) + contest_url
+		} else {
+			contest_map["url"] = contest_url
+		}
+
 		contests[i] = contest_map
 	})
 	return contests
 }
 
-func getContestsArtefactos(contests_elem *goquery.Selection, contests pageContestsData) pageContestsData {
+func getContestsArtefactos(contests_elem *goquery.Selection, contests pageContestsData, _ string) pageContestsData {
 	contests_elem.Each(func(i int, s *goquery.Selection) {
 		contest_map := make(map[string]string)
 		contest_map["name"] = s.Find("h4").Text()
@@ -103,7 +128,8 @@ func getContestsArtefactos(contests_elem *goquery.Selection, contests pageContes
 	return contests
 }
 
-func getContestsTransporteslisboa(contests_elem *goquery.Selection, contests pageContestsData) pageContestsData {
+/*
+func getContestsAXN(contests_elem *goquery.Selection, contests pageContestsData) pageContestsData {
 	contests_elem.Each(func(i int, s *goquery.Selection) {
 		contest_map := make(map[string]string)
 		contest_map["name"] = s.Text()
@@ -112,11 +138,7 @@ func getContestsTransporteslisboa(contests_elem *goquery.Selection, contests pag
 	})
 	return contests
 }
-
-func getContestsAntena3(contests_elem *goquery.Selection, contests pageContestsData) pageContestsData {
-	return getContestsTransporteslisboa(contests_elem, contests)
-}
-
+*/
 
 
 func convertToJson(to_convert contestsData) string {
@@ -138,4 +160,10 @@ func mergeMaps(contests_list []contestsData) contestsData {
 	}
 
 	return final_map
+}
+
+// "http://activa.sapo.pt/passatempos/" => "http://activa.sapo.pt/"
+func getURLroot(url string) string {
+	splitted_url := strings.SplitAfter(url, "/")
+	return strings.Join(splitted_url[0:3],"")
 }
