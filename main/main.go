ceb85bb2291b8285563670e79d0412b4e9a0b598
LLCampos <luis1212@sapo.pt>
2017-08-06 12:14:27 +0100

    Add content type in response header

--- main/main.go
+++ main/main.go
@@ -9,10 +9,11 @@ import (
 
 func contestsHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
     io.WriteString(w, gopscraper.GetContests())
 }
 
 func main() {
     http.HandleFunc("/passatempos", contestsHandler)
-    http.ListenAndServe(":5000", nil)
+    http.ListenAndServe(":5001", nil)
 }
